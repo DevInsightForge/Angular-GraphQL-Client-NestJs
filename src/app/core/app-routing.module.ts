f6f7cd00344ed077616f1a96a4cef6a056b23ab9
ZèD <imzihad@gmail.com>
2023-04-08 07:26:49 +0600

    app: added component route links
    Signed-off-by: ZèD <imzihad@gmail.com>

--- src/app/core/app-routing.module.ts
+++ src/app/core/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MessagesComponent } from 'src/app/components/messages/messages.component';
 import { LoginComponent } from '../components/login/login.component';
+import { RegisterComponent } from '../components/register/register.component';
 
 const routes: Routes = [
   {
@@ -14,6 +15,10 @@ const routes: Routes = [
       {
         path: '',
         component: LoginComponent,
+      },
+      {
+        path: 'register',
+        component: RegisterComponent,
       },
     ],
   },
